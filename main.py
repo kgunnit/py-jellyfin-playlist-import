af3890330664d78702d1b08449c60f1cb36e7e71
Jonas Heinrich <onny@project-insanity.org>
2019-03-06 17:18:44 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+import requests
+import argparse
+import os
+import json
+import sys
+
+# fixme detect http / https in host
+# fixme requests timeout + error log
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file", help="m3u8 playlist input file")
+parser.add_argument("playlist_name", help="playlist name to create in jellyfin")
+parser.add_argument("--host", help="host ip or domain name")
+parser.add_argument("--userid", help="userid hash")
+parser.add_argument("--apikey", help="api key hash")
+args = parser.parse_args()
+
+host = args.host
+userid = args.userid
+apikey = args.apikey
+
+track_query_url = "https://" + host + "/Items?UserId="+userid+"&format=json&api_key="+apikey+"&Recursive=true&IncludeItemTypes=Audio%2CMusicAlbum%2CMusicArtist&SearchTerm="
+create_playlist_url = "https://" + host + "/Playlists?UserId="+userid+"&api_key="+apikey+"&Name="
+add_playlist_url = "https://" + host + "/Playlists/"
+
+input_file = args.input_file
+
+def logmsg(status,msg):
+    if (status == "info"):
+        print("[*] "+msg)
+    elif (status == "error"):
+        print(bcolors.FAIL + "[!] "+msg + bcolors.ENDC)
+    elif (status == "success"):
+        print(bcolors.OKGREEN + "[+] "+msg + bcolors.ENDC)
+
+playlist_name = args.playlist_name
+logmsg("info","Creating playlist: "+playlist_name)
+playlist_response = requests.post(create_playlist_url + playlist_name)
+try:
+    playlist_response_json = json.loads(playlist_response.text)
+    playlist_id = playlist_response_json["Id"]
+    logmsg("success","Playlist created with Id: "+playlist_id)
+except:
+    logmsg("fail","Creating playlist failed")
+    sys.exit(0)
+
+with open(input_file, "r") as input_file:
+    for media_file in input_file:
+        media_file = media_file.rstrip()
+        if (media_file[0] != "#"):
+            media_file_fullpath = media_file
+            media_file = os.path.basename(media_file)
+            media_file_name = os.path.splitext(media_file)[0]
+            query_url = track_query_url + media_file_name
+            logmsg("info","Querying track: "+media_file_name)
+            response = requests.get(query_url)
+            response_json = json.loads(response.text)
+            if (len(response_json["Items"]) is not 0):
+                for response_item in response_json["Items"]:
+                    response_path = response_item["Path"]
+                    if (response_path == media_file_fullpath):
+                        response_id = response_item["Id"]
+                        logmsg("success","  Found match with track ID: "+response_id)
+                        logmsg("info","  Adding to playlist ...")
+                        data = {"Ids": [ response_id ]}
+                        playlist_add_response = requests.post(add_playlist_url+playlist_id+"/Items?Ids="+response_id+"&UserId="+userid+"&api_key="+apikey)
+                        if (playlist_add_response.status_code == 200):
+                            logmsg("success","  Track successfully added to playlist")
+                        else:
+                            logmsg("fail","  Unable to add track")
+            else:
+                logmsg("fail","  Unable to find track")
